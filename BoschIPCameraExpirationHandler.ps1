5b8d67496339be6fb2e862e85da4994cb535a9e1
Macey Dobrowsky <macey.dobrowsky@keyfactor.com>
2024-10-24 04:02:08 +0000

    rework of sample expiration handler

---
+++ BoschIPCameraExpirationHandler.ps1
@@ -0,0 +1,361 @@
+﻿# // Copyright 2024 Keyfactor
+# // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+# // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+# // and limitations under the License.
+
+[hashtable]$context
+$Thumb = $context["Thumbprint"]
+$CA = $context["CAConfiguration"]
+$Template = $context["Template"]
+
+# script variables
+#############################################################
+$apiUrl = "https://my.keyfactor.instance01/KeyfactorAPI" # update to be Keyfactor API endpoint
+$LogDest = "C:\Keyfactor\logs\BoschIPCameraExpirationHandler.log" # the location for the error log file, the script will create this file
+#############################################################
+
+Function LogWrite($LogString)
+{
+    Add-Content $LogDest -value $LogString
+}
+
+Function GetIdAndSans
+{
+    try
+    {
+        $searchURL = $apiUrl + "/certificates/?verbose=1&maxResults=50&page=1&query=Thumbprint%20-eq%20`""+$Thumb+"`""
+
+        $certificateResponse = Invoke-RestMethod `
+        -Method Get `
+        -Uri $searchUrl `
+        -UseDefaultCredentials `
+        -ContentType "application/json"
+
+        $certValues = @{}
+
+        $certValues.Id = $certificateResponse.Id
+        # Note: SANs not currently supported for Bosch IP Camera Reenrollment
+        # $certValues.Sans = ""
+
+        # foreach($san in $certificateResponse.SubjectAltNameElements)
+        # {
+        #     if ($san.Type -eq 2) #include only dns sans (type 2)
+        #     {
+        #         if ($cert.Sans)
+        #         {
+        #             $certValues.Sans = $certValues.Sans + "&" # add ampersand delimiter for additional DNS SANs
+        #         }
+        #         $certValues.Sans = $certValues.Sans + $san.Value
+        #     }
+        # }
+
+        return $certValues
+    }
+    catch
+    {
+        LogWrite "An error occurred looking up the certificate in Keyfactor"
+        LogWrite $_
+        return "SEARCH_ERROR"        
+    }
+}
+
+Function GetLocations($certId)
+{
+    try
+    {
+        $locationsURL = $apiUrl + "/certificates/locations/" + $certId
+        $locationsResponse = Invoke-RestMethod `
+        -Method Get `
+        -Uri $locationsURL `
+        -UseDefaultCredentials `
+        -ContentType "application/json"
+
+        foreach($storetype in $locationsResponse.Details)
+        {
+            if ($storetype.StoreType -match "BIPCamera")
+            {
+                return $storetype.Locations # array of all Bosch IP Camera locations this cert is in
+            }
+        }
+    }
+    catch
+    {
+        LogWrite "An error occurred looking up the certificate locations in Keyfactor"
+        LogWrite $_
+        return "LOCATIONS_ERROR"
+    }
+}
+
+Function GetOrchestratorId($storeId)
+{
+    try
+    {
+        $storeURL = $apiUrl + "/certificatestores/" + $storeId
+        $storeResponse = Invoke-RestMethod `
+        -Method Get `
+        -Uri $storeURL `
+        -UseDefaultCredentials `
+        -ContentType "application/json"
+
+        return $storeResponse.AgentId
+    }
+    catch
+    {
+        LogWrite "An error occurred while retrieving the Orchestrator ID of the store with id: " + $storeId
+        LogWrite $_
+        return "GET_ORCHESTRATORID_ERROR"
+    }
+}
+
+Function GetStoreInventory($storeId)
+{
+    try
+    {
+        $inventoryURL = $apiUrl + "/certificatestores/" + $storeId + "/inventory"
+        $inventoryResponse = Invoke-RestMethod `
+        -Method Get `
+        -Uri $inventoryURL `
+        -UseDefaultCredentials `
+        -ContentType "application/json"
+
+        return $inventoryResponse #
+    }
+    catch
+    {
+        LogWrite "An error occurred while retrieving the inventory of certs in the store with id: " + $storeId
+        LogWrite $_
+        return "GET_INVENTORY_ERROR"
+    }
+}
+
+Function FindInventoryParameters($inventoryList)
+{
+    try
+    {
+        $parsedResults = @{}
+        # search through the store inventory list for matching cert
+        foreach($inventoryItem in $inventoryList)
+        {
+            if ($inventoryItem.Name -eq $Thumb)
+            {
+                # get cert subject
+                foreach ($cert in $inventoryItem.Certificates)
+                {
+                    if ($cert.Id -eq $CertId)
+                    {
+                        $parsedResults.Subject = $cert.IssuedDN
+                    }
+                }
+                $parsedResults.Parameters = $inventoryItem.Parameters
+                return $parsedResults
+            }
+        }
+    }
+    catch
+    {
+        LogWrite "An error occurred while parsing the job parameters from the cert store inventory"
+        LogWrite $_
+        return "PARSE_INVENTORY_ERROR"
+    }
+}
+
+Function FilterSubjectForBoschIPCamera($certSubject)
+{
+    # Subject fields allowed by Bosch IP Camera:
+    # SERIALNUMBER, CN, C, L, O, OU, ST
+    $subjectElements = $certSubject.split(',')
+
+    $parsedSubject = ""
+
+    $noSerialNumber? = $true
+    $noCN? = $true
+    $noC? = $true
+    $noL? = $true
+    $noO? = $true
+    $noOU? = $true
+    $noST? = $true
+    foreach($ele in $subjectElements)
+    {
+        if ($noSerialNumber?)
+        {
+            if ($ele -match 'SERIALNUMBER=')
+            {
+                $parsedSubject += $ele + ','
+                $noSerialNumber? = $false
+            }
+        }
+
+        if ($noCN?)
+        {
+            if ($ele -match 'CN=')
+            {
+                $parsedSubject += $ele + ','
+                $noCN? = $false
+            }
+        }
+        if ($noC?)
+        {
+            if ($ele -match 'C=')
+            {
+                $parsedSubject += $ele + ','
+                $noC? = $false
+            }
+        }
+        if ($noL?)
+        {
+            if ($ele -match 'L=')
+            {
+                $parsedSubject += $ele + ','
+                $noL? = $false
+            }
+        }
+        if ($noO?)
+        {
+            if ($ele -match 'O=')
+            {
+                $parsedSubject += $ele + ','
+                $noO? = $false
+            }
+        }
+        if ($noOU?)
+        {
+            if ($ele -match 'OU=')
+            {
+                $parsedSubject += $ele + ','
+                $noOU? = $false
+            }
+        }
+        if ($noST?)
+        {
+            if ($ele -match 'ST=')
+            {
+                $parsedSubject += $ele + ','
+                $noST? = $false
+            }
+        }
+    }
+
+    # trim comma at end
+    $i = $parsedSubject.LastIndexOf(',')
+    return $parsedSubject.Substring(0, $i)
+}
+
+Function ScheduleReenrollment($storeId, $orchId, $inventoryList)
+{
+    try
+    {        
+        # parse inventory parameters for reenrollment, and get subject
+        $reenrollmentParameters = FindInventoryParameters($inventoryList)
+        LogWrite "Parsed Subject: " 
+        $Subject = $reenrollmentParameters.Subject
+        LogWrite $Subject
+        LogWrite "Subject filtered for Bosch IP Camera Reenrollment: "
+        $FilteredSubject = FilterSubjectForBoschIPCamera($Subject)
+        LogWrite $FilteredSubject
+        LogWrite "Parsed Inventory Parameters"
+        $Parameters = $reenrollmentParameters.Parameters
+
+        # set Overwrite to True to make sure the new certificate can replace the existing one with the same name
+        $Parameters.Overwrite = $true
+
+        # add sans, which are not returned on inventory
+        # $Parameters.Sans = $sans
+        LogWrite $Parameters
+
+        # escape backslashes in CA name
+        $escapedCA = $CA -replace '\\', '\\'
+
+        # convert Parameters object to Json
+        $paramsJSON = $Parameters | ConvertTo-Json
+
+        # get Orchestrator Agent Id for scheduling reenrollment
+
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add('content-type', 'application/json')
+        $headers.Add("X-Keyfactor-Requested-With", "APIClient")
+        $body = @"
+{
+  "KeystoreId": "$storeId",
+  "SubjectName": "$FilteredSubject",
+  "AgentGuid": "$orchId",
+  "Alias": "$Thumb",
+  "JobProperties": $paramsJson,
+  "CertificateAuthority": "$escapedCA",
+  "CertificateTemplate": "$Template"
+}
+"@
+        LogWrite $body
+        $reenrollmentURL = $apiUrl + "/certificatestores/reenrollment"
+        $reenrollmentResponse = Invoke-RestMethod `
+        -Method Post `
+        -Uri $reenrollmentURL `
+        -Headers $headers `
+        -UseDefaultCredentials `
+        -Body $body `
+        -ContentType "application/json"
+
+    }
+    catch
+    {
+        LogWrite "An error occurred while scheduling the reenrollment job"
+        LogWrite $_
+        return "REENROLLMENT_ERROR"
+    }
+}
+
+try
+{
+    LogWrite (Get-Date).ToUniversalTime().ToString()
+    LogWrite $Thumb
+    LogWrite $CA
+    LogWrite $Template
+    LogWrite $CertLocations
+    LogWrite $context
+}
+catch
+{
+    LogWrite "An error occurred reading info into the logs"
+    LogWrite $_
+    return "INFO_ERROR"
+}
+
+try
+{
+    # get CertID and Sans
+    $CertValues = GetIdAndSans
+    $CertId = $CertValues.Id
+    # $CertSans = $CertValues.Sans
+
+    LogWrite $CertId
+    # LogWrite $CertSans
+
+            
+    # get cert locations
+    $Locations = GetLocations($CertId)
+    LogWrite "Retrieved Locations"
+
+    # process for each store location this cert is in
+    foreach($Location in $Locations)
+    {
+        $StoreId = $Location.StoreId
+
+        # get Orchestrator Id for store
+        $OrchestratorId = GetOrchestratorId $StoreId
+
+        # get inventory of location
+        $InventoryList = GetStoreInventory $StoreId
+        LogWrite "Got Store Inventory Parameters"
+        LogWrite $InventoryList
+
+        # schedule reenrollment with parameters
+        ScheduleReenrollment $StoreId $OrchestratorId $InventoryList
+        LogWrite "Scheduled Reenrollment"
+    }
+}
+catch
+{
+    LogWrite (Get-Date).ToUniversalTime().ToString()
+    LogWrite "Script Failed Gracefully"
+}
